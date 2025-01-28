6d4c6576a00c7e661fc14294af4275a86ab12d48
selina tao <161782379+selinatao@users.noreply.github.com>
2025-01-28 02:52:31 -0600

    Added project files

---
+++ main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+/*javascript comment*/
+//also comment same as java basically
+//uses === instead of == and != is !==
+//everything mostly same as python/java 
